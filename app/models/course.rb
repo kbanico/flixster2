b600014dc320791e1800f6961f3191c2eb309070
kbanico <code4uguam@gmail.com>
2017-04-05 11:31:24 +0000

    add section model, section controller, new, and create action, and show section on course show page

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,6 @@
 class Course < ApplicationRecord
   belongs_to :user
+  has_many :sections
   validates :title, presence: true, length: {minimum:3, maximum:100}
   validates :description, presence: true, length: {minimum:3, maximum:100}
   validates :cost, presence: true, numericality: {greater_than_or_equal_to: 0}
