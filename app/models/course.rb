e0d1a93c5d213c1adc570fe23e12fdae57e39447
kbanico <code4uguam@gmail.com>
2017-04-05 01:10:33 +0000

    add validations to courses model

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,6 @@
 class Course < ApplicationRecord
   belongs_to :user
+  validates :title, presence: true, length: {minimum:3, maximum:100}
+  validates :description, presence: true, length: {minimum:3, maximum:100}
+  validates :cost, presence: true, numericality: {greater_than_or_equal_to: 0}
 end
