de9abc44d705655a47f947c49eff9f36c11a237d
kbanico <code4uguam@gmail.com>
2017-04-08 11:53:29 +0000

    Setting up the Course Detail & Video Page

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -8,8 +8,8 @@ class Instructor::SectionsController < ApplicationController
 
   def create
     #@course = Course.find(params[:course_id])
-    @section = @course.sections.create(section_params)
-    redirect_to instructor_course_path(@course)
+    current_course.sections.create(section_params)
+    redirect_to instructor_course_path(current_course)
   end
 
   helper_method :current_course
