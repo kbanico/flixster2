7420edd836b4bff4bdb69cfb3baea8978b1683d1
kbanico <code4uguam@gmail.com>
2017-04-19 22:30:46 +0000

    add enrollment functionality

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,7 @@
 class Course < ApplicationRecord
   belongs_to :user
   has_many :sections
+  has_many :enrollments
   validates :title, presence: true, length: {minimum:3, maximum:100}
   validates :description, presence: true, length: {minimum:3, maximum:100}
   validates :cost, presence: true, numericality: {greater_than_or_equal_to: 0}
