e629a8ad755e581f99ea2af7dcda0d9b098b59fc
kbanico <code4uguam@gmail.com>
2017-04-21 09:19:52 +0000

    style instructor course show page

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -1,6 +1,8 @@
 class Instructor::SectionsController < ApplicationController
   before_action :authenticate_user!
   before_action :require_authorized_for_current_course
+  before_action :require_authorized_for_current_section, only: [:update]
+
   def new
     #@course = Course.find(params[:course_id])
     @section = Section.new
@@ -12,14 +14,34 @@ class Instructor::SectionsController < ApplicationController
     redirect_to instructor_course_path(current_course)
   end
 
+  def update
+    current_section.update_attributes(section_params)
+    render plain: "updated!"
+  end
+
   helper_method :current_course
   def current_course
-    @current_course ||= Course.find(params[:course_id])
+    if params[:course_id]
+      @current_course ||= Course.find(params[:course_id])
+    else
+      current_section.course
+    end
   end
 
   private
+
+    def require_authorized_for_current_section
+      if current_section.course.user != current_user
+        render text: "Unauthorized", status: :unauthorized
+      end
+    end
+
+    def current_section
+      @current_section ||= Section.find(params[:id])
+    end
+
     def section_params
-      params.require(:section).permit(:title)
+      params.require(:section).permit(:title, :row_order_position)
     end
 
     def require_authorized_for_current_course
